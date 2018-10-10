a126a997c954c9d09449387ad94c2d4163ba396e
Tom Ridge <tom.j.ridge@googlemail.com>
2018-10-10 17:03:36 +0100

    +

--- src/pcache_debug.ml
+++ src/pcache_debug.ml
@@ -1,3 +1,3 @@
 open Tjr_log
-let log = Tjr_fs_shared.Log.log_ops.log
-let log_lazy = Tjr_fs_shared.Log.log_ops.log_lazy
+let log = Tjr_fs_shared.Fs_log.log_ops.log
+let log_lazy = Tjr_fs_shared.Fs_log.log_ops.log_lazy
