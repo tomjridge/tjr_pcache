70408d65b37185109677302a2a890e96feeaf846
Tom Ridge <tom.j.ridge@googlemail.com>
2018-12-14 13:27:45 +0000

    +

--- bin/run_dmap_example.ml
+++ bin/run_dmap_example.ml
@@ -4,8 +4,18 @@ open Tjr_pcache
 let fn = "dmap_example.store"
 let count = int_of_string Sys.argv.(1)
 
+let now =
+  Core.(fun () ->
+      Time_stamp_counter.(now () |> to_time_ns)
+      |>Time_ns.to_int63_ns_since_epoch 
+      |>Int63.to_int_exn)
+
 let _ = 
-  Dmap_example.test_dmap_ops_on_file ~fn ~count
+  Printf.printf "%s: starting write... %!" __MODULE__;
+  let now1 = now() in
+  Dmap_example.test_dmap_ops_on_file ~fn ~count;
+  let time = now() - now1 in
+  Printf.printf "finished in %d\n%!" time
 
 let pow =
   let rec pow' a x n =
@@ -13,8 +23,14 @@ let pow =
   pow' 1
 
 let _ =
+  Printf.printf "%s: starting read... %!" __MODULE__;
+  let now1 = now() in
   Dmap_example.read_back ~fn |> fun ess ->
-  Printf.printf "%s: read back %d ops\n%!" __MODULE__ (List.length (List.concat ess));
+  let time = now() - now1 in
+  Printf.printf 
+    " read back %d ops in %d\n%!" 
+    (List.length (List.concat ess)) 
+    time;
   
 (*
 let open open Ins_del_op_type in
@@ -40,6 +56,12 @@ let open open Ins_del_op_type in
 (*
 
 100k in 2.2s
-1M fails with stackoverflow - presumably a list op somewhere is not tail rec? culprit seems to be Tjr_list.from_to
+
+1M:
+Run_dmap_example: starting write... finished in 27924296012  
+ie 27 s to write
+
+Run_dmap_example: starting read...  read back 1000000 ops in 189117339
+.18s to read
 
 *)
