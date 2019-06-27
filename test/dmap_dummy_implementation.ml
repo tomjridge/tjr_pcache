b80afacaa3dc6a14227d7f8c1480a8761c37b3f4
Tom Ridge <tom.j.ridge@googlemail.com>
2019-06-27 18:41:04 +0100

    resurrect dmap_dummy_implementation; wip to add others

--- test/dmap_dummy_implementation.ml
+++ test/dmap_dummy_implementation.ml
@@ -7,23 +7,23 @@ For block_list_length, we store the number of ops and perform some
 
 open Tjr_map
 
-open Ins_del_op_type
+open Ins_del_op
 
 
 module Dummy_state = struct
   type ('k,'v,'ptr) t = {
-    map_past: ('k,('k,'v)op) Tjr_polymap.t;
-    map_current: ('k,('k,'v)op) Tjr_polymap.t;
+    map_past: ('k,('k,'v)op,unit) Tjr_map.map;
+    map_current: ('k,('k,'v)op,unit) Tjr_map.map;
     op_count_current: int;
     ptr_current: 'ptr;
     block_list_length: int;
   }
 
   let init_dummy_state ~init_ptr = 
-    let map_ops = Op_aux.default_kvop_map_ops () in
+    let map_ops = Tjr_map.make_map_ops Pervasives.compare in
     {
-      map_past = map_ops.map_empty;
-      map_current = map_ops.map_empty;
+      map_past = map_ops.empty;
+      map_current = map_ops.empty;
       op_count_current=0;
       ptr_current=init_ptr;
       block_list_length=1
@@ -33,17 +33,19 @@ end
 
 open Dummy_state
 
+let map_merge = Detachable_map.Internal.map_merge
+
 let make_dmap_ops ~monad_ops ~with_state ~ops_per_block ~alloc_ptr
   : ('k,'v,'ptr,'t)Dmap_types.dmap_ops 
   = 
   let return = monad_ops.return in
   let ( >>= ) = monad_ops.bind in
-  let map_ops = Op_aux.default_kvop_map_ops () in
+  let map_ops = Tjr_map.make_map_ops Pervasives.compare in
   let with_state = with_state.with_state in
   let find k =
-    with_state (fun ~state:s ~set_state -> 
-        let map = Tjr_map.map_union ~map_ops ~m1:s.map_past ~m2:s.map_current in
-        let op = map_ops.map_find k map in
+    with_state (fun ~state:s ~set_state:_ -> 
+        let map = map_merge ~map_ops ~old:s.map_past ~new_:s.map_current in
+        let op = map_ops.find_opt k map in
         return (
           match op with
           | None -> None
@@ -53,17 +55,17 @@ let make_dmap_ops ~monad_ops ~with_state ~ops_per_block ~alloc_ptr
   let maybe_move_to_new_block s = 
     match s.op_count_current > ops_per_block with
     | true -> alloc_ptr () >>= fun ptr -> return {
-        map_current=map_ops.map_empty; 
+        map_current=map_ops.empty; 
         op_count_current=0;
         ptr_current=ptr;
-        map_past=(Tjr_polymap.union s.map_past s.map_current);
+        map_past=(map_merge ~map_ops ~old:s.map_past ~new_:s.map_current);
         block_list_length=s.block_list_length+1 }
     | false -> return s
   in
   let insert k v = 
     with_state (fun ~state:s ~set_state -> 
         let s = { s with 
-                  map_current=map_ops.map_add k (Insert(k,v)) s.map_current;
+                  map_current=map_ops.add k (Insert(k,v)) s.map_current;
                   op_count_current=s.op_count_current+1;
                 }
         in
@@ -74,7 +76,7 @@ let make_dmap_ops ~monad_ops ~with_state ~ops_per_block ~alloc_ptr
   let delete k =
     with_state (fun ~state:s ~set_state -> 
         let s = { s with 
-                  map_current=map_ops.map_add k (Delete(k)) s.map_current;
+                  map_current=map_ops.add k (Delete(k)) s.map_current;
                   op_count_current=s.op_count_current+1;
                 }
         in
@@ -85,7 +87,7 @@ let make_dmap_ops ~monad_ops ~with_state ~ops_per_block ~alloc_ptr
   let detach () =
     with_state (fun ~state:s ~set_state -> 
         let new_state = { 
-          map_past=map_ops.map_empty;
+          map_past=map_ops.empty;
           map_current=s.map_current;
           op_count_current=s.op_count_current;
           ptr_current=s.ptr_current;
@@ -98,7 +100,7 @@ let make_dmap_ops ~monad_ops ~with_state ~ops_per_block ~alloc_ptr
           current_map=s.map_current; 
           current_ptr=s.ptr_current })
   in
-  let block_list_length () = with_state (fun ~state:s ~set_state -> 
+  let block_list_length () = with_state (fun ~state:s ~set_state:_ -> 
       return s.block_list_length)
   in
   Dmap_types.{find;insert;delete;detach;block_list_length}
